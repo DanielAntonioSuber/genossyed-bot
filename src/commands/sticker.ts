c92aebdc2efc28a0b91170754516ca5cee1ba846
DanielAntonioSuber <antonio.eugenio.daniel1@gmail.com>
2023-07-05 22:00:01 -0600

    sticker

--- src/commands/sticker.ts
+++ src/commands/sticker.ts
@@ -1,43 +1,75 @@
 import { WASocket, downloadMediaMessage, proto } from "@whiskeysockets/baileys";
 import { pino } from "pino";
-import Sticker from "wa-sticker-formatter";
+import Sticker, { StickerTypes } from "wa-sticker-formatter";
 
-export async function sticker (webMessageInfo: proto.IWebMessageInfo, sock: WASocket) {
+export async function sticker(webMessageInfo: proto.IWebMessageInfo, sock: WASocket) {
   const quotedImageMessage = webMessageInfo.message?.extendedTextMessage?.contextInfo?.quotedMessage?.imageMessage
+  const quotedVideoMessage = webMessageInfo.message?.extendedTextMessage?.contextInfo?.quotedMessage?.videoMessage
   const id = webMessageInfo.key.remoteJid
-  if(!id) return
-  
-  if(quotedImageMessage) {
+  if (!id) return
+
+  if (quotedImageMessage) {
     const messageInfo: proto.IWebMessageInfo = {
       key: {
         id: quotedImageMessage.contextInfo?.stanzaId,
         participant: webMessageInfo.key.participant,
         remoteJid: webMessageInfo.key.remoteJid
       },
-      message: {imageMessage: quotedImageMessage}
+      message: { imageMessage: quotedImageMessage }
     }
-    const logger = pino({ level: 'debug'})
-    const buffer = await downloadMediaMessage(messageInfo, 'buffer', {}, {logger, reuploadRequest: sock.updateMediaMessage, })
-    
-    if(buffer instanceof Buffer) {
+    const logger = pino({ level: 'debug' })
+    const buffer = await downloadMediaMessage(messageInfo, 'buffer', {}, { logger, reuploadRequest: sock.updateMediaMessage, })
+
+    if (buffer instanceof Buffer) {
       const sticker = await (
         new Sticker(
-            buffer, 
-            { 
-              pack: 'Bot PACK',
-              author: 'BOT',
-              categories: ['☔️', '❤️'], 
-              id: 'BOTSYED-', 
-              quality: 90, 
-              background:  '#000000'
-            }
-          )
-          .toMessage()
+          buffer,
+          {
+            pack: 'Thelmstone',
+            author: 'B7',
+            categories: ['☔️', '❤️'],
+            id: 'BOTSYED-',
+            quality: 99,
+            background: '#00000000',
+            type: StickerTypes.FULL
+          }
         )
-      
-      await sock.sendMessage(id, sticker, {quoted: webMessageInfo})
+          .toMessage()
+      )
+
+      await sock.sendMessage(id, sticker, { quoted: webMessageInfo })
+    }
+  } else if (quotedVideoMessage) {
+    const messageInfo: proto.IWebMessageInfo = {
+      key: {
+        id: quotedVideoMessage.contextInfo?.stanzaId,
+        participant: webMessageInfo.key.participant,
+        remoteJid: webMessageInfo.key.remoteJid
+      },
+      message: { videoMessage: quotedVideoMessage }
+    }
+    const logger = pino({ level: 'debug' })
+    const buffer = await downloadMediaMessage(messageInfo, 'buffer', {}, { logger, reuploadRequest: sock.updateMediaMessage, })
+
+    if (buffer instanceof Buffer) {
+      const sticker = await (
+        new Sticker(
+          buffer,
+          {
+            pack: 'Thelmstone',
+            author: 'B7',
+            categories: ['☔️', '❤️'],
+            id: 'BOTSYED-',
+            quality: 15,
+            background: '#00000000',
+            type: StickerTypes.FULL
+          }
+        )
+          .toMessage()
+      )
+      await sock.sendMessage(id, sticker, { quoted: webMessageInfo })
     }
   } else {
-    await sock.sendMessage(id,{text: 'No es una imágen' }, {quoted: webMessageInfo})
+    await sock.sendMessage(id, { text: 'No es una imágen' }, { quoted: webMessageInfo })
   }
 }
--- src/handleComands.ts
+++ src/handleComands.ts
@@ -20,7 +20,20 @@ function handleCommands(sock: WASocket) {
                 console.log('replying to', m.messages[0].key.remoteJid)
                 console.log(textMessage);
                 await sock.sendMessage(m.messages[0].key.remoteJid!, { text: 'Isixto es un ser anormal y estúpido creador de la inmunda frase "Producto por escalera".' }, {quoted: m.messages[0]})
-            }
+            } else if(textMessage.conversation == "!tesan") {
+                console.log('replying to', m.messages[0].key.remoteJid)
+                console.log(textMessage);
+                await sock.sendMessage(m.messages[0].key.remoteJid!, { text: 'Tesan es un buzonero de mierda, pero prefieren que lo llamen "phreaker", no tiene vida social y sólo roba números de lugares que ni Dios se acuerda que hizo, es venezolano emigrado a España' }, {quoted: m.messages[0]})
+            } else if(textMessage.conversation == "!zlider") {
+                console.log('replying to', m.messages[0].key.remoteJid)
+                console.log(textMessage);
+                await sock.sendMessage(m.messages[0].key.remoteJid!, { text: 'El famosisimo gordito cariñosito que hizo el video de "yo y elias tuvimos sexo" fanático del trap latino y de la comida rápida, tiene chaleco antibalas con 7 kilos de manteca abdominal, ¡POR DIOS, PUEDE FRENAR UN CARGADOR COMPLETO DE UNA M4A1! Zlider. El mejor osito cariñosito color aceite de carro quemado.' }, {quoted: m.messages[0]})
+            } else if(textMessage.conversation == "!nik") {
+                console.log('replying to', m.messages[0].key.remoteJid)
+                console.log(textMessage);
+                await sock.sendMessage(m.messages[0].key.remoteJid!, { text: 'MEXICANO BASURA, EDGY Y FAN DE EVANGELION, NO HABLA EN LAS LLAMADAS PORQUE CREE QUE CON SU VOZ LE VAN A HACER UN DOXX SONORO-GEOLOGICO Y DARÁN CON SU CHOZA EN CHAPULTEPEC.' }, {quoted: m.messages[0]})
+            } 
+
         }
     }
     
